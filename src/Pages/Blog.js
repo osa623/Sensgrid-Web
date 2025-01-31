f7cb6abe0ec1c29941b732d41c9b6a508be919fc
osa623 <it22170798@my.sliit.lk>
2025-02-01 04:18:27 +0530

    commit

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -11,15 +11,15 @@ const Blog = () => {
   return (
 
     <div className='relative h-auto w-auto justify-center items-center'>
-      <div className='flex lgs:h-[8rem] w-full h-auto'/>
+      <div className='flex lgs:h-[8rem] sms:h-[5rem] w-full'/>
 
         <div className="flex flex-col lgs:w-[75rem] mx-auto p-4 bg-white rounded-lg shadow-lg">
 
         <div className='flex lgs:w-[70rem] justify-between items-center'>
-            <h1 className="lgs:text-5xl font-bold text-blue-900" style={{
+            <h1 className="lgs:text-5xl sms:text-2xl sms:font-bold text-blue-900" style={{
                 fontWeight:'500'
               }}>{blog.title}</h1>
-              <p className="text-md text-gray-600">{blog.date}</p>
+              <p className="hidden lgs:flex text-md text-gray-600">{blog.date}</p>
                
         </div>
 
@@ -30,10 +30,11 @@ const Blog = () => {
             <div>
               <h3 className="text-lg font-semibold">{blog.authorName}</h3>
               <p className="text-xs text-gray-600">{blog.authorTitle}</p>
+              <p className="hidden sms:flex text-xs text-gray-600">{blog.date}</p>
             </div>
           </div>
 
-          <img src={blog.image} alt={blog.title} className="lgs:w-[50rem] lgs:mt-5 rounded-lg mb-4" style={{
+          <img src={blog.image} alt={blog.title} className="lgs:w-[50rem] mt-5 rounded-lg mb-4" style={{
             boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
           }}/>
           <div className="text-gray-800 lgs:w-[70vw] space-y-4">
@@ -45,23 +46,23 @@ const Blog = () => {
 
           <div className='flex flex-col h-auto w-auto lgs:mt-8'>
 
-          <div className='flex lgs:w-[70rem] justify-between items-center lgs:mt-4'>
-            <h1 className="lgs:text-3xl font-bold text-blue-900" style={{
+          <div className='flex lgs:w-[70rem] justify-between items-center lgs:mt-4 sms:mt-5'>
+            <h1 className="lgs:text-3xl sms:text-xl  font-bold text-blue-900" style={{
                 fontWeight:'500'
               }}>{blog.topic2}</h1>              
           </div>
-         <img src={blog.subimage01} alt={blog.title} className="lgs:w-[50rem] lgs:mt-5 rounded-lg mb-4" style={{
+         <img src={blog.subimage01} alt={blog.title} className="lgs:w-[50rem] mt-5 rounded-lg mb-4" style={{
             boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
           }}/>
             
             
-              <h2 className=" lgs:w-[70vw] lgs:text-2xl  text-secondary">{blog.subtopic1}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-2xl sms:text-xl   text-secondary">{blog.subtopic1}</h2>
 
-              <h2 className=" lgs:w-[70vw] lgs:text-md lgs:mt-5  text-secondary">{blog.subdescription1}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-md mt-5  text-secondary">{blog.subdescription1}</h2>
 
-              <h2 className=" lgs:w-[70vw] lgs:text-2xl lgs:mt-5  text-secondary">{blog.subtopic12}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-2xl sms:text-xl mt-5  text-secondary">{blog.subtopic12}</h2>
 
-              <div className="text-gray-800 lgs:w-[50vw] lgs:mt-5 space-y-4">
+              <div className="text-gray-800 lgs:w-[50vw] mt-5 space-y-4">
               {blog.subdescription12.map((paragraph, index) => (
                 <p key={index}>
                   {/* Split the paragraph into parts to bold the first part */}
@@ -75,33 +76,28 @@ const Blog = () => {
 
             </div>
 
-            
-         
-
-
-
           </div>
 
 
-          <div className='flex flex-col h-auto w-auto lgs:mt-8'>
+          <div className='flex flex-col h-auto w-auto mt-8'>
 
           <div className='flex lgs:w-[70rem] justify-between items-center lgs:mt-4'>
-            <h1 className="lgs:text-3xl font-bold text-blue-900" style={{
+            <h1 className="lgs:text-3xl sms:text-xl font-bold text-blue-900" style={{
                 fontWeight:'500'
               }}>{blog.topic2}</h1>              
           </div>
-          <img src={blog.subimage02} alt={blog.title} className="lgs:w-[50rem] lgs:mt-5 rounded-lg mb-4" style={{
+          <img src={blog.subimage02} alt={blog.title} className="lgs:w-[50rem] mt-5 rounded-lg mb-4" style={{
             boxShadow: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
           }}/>
             
             
-              <h2 className=" lgs:w-[70vw] lgs:text-2xl  text-secondary">{blog.subtopic21}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-2xl  sms:text-xl text-secondary">{blog.subtopic21}</h2>
 
-              <h2 className=" lgs:w-[70vw] lgs:text-md lgs:mt-5  text-secondary">{blog.subdescription21}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-md mt-5  text-secondary">{blog.subdescription21}</h2>
 
-              <h2 className=" lgs:w-[70vw] lgs:text-2xl lgs:mt-5  text-secondary">{blog.subtopic22}</h2>
+              <h2 className=" lgs:w-[70vw] lgs:text-2xl sms:text-xl mt-5  text-secondary">{blog.subtopic22}</h2>
 
-              <div className="text-gray-800 lgs:w-[50vw] lgs:mt-5 space-y-4">
+              <div className="text-gray-800 lgs:w-[50vw] mt-5 space-y-4">
               {blog.subdescription22.map((paragraph, index) => (
                 <p key={index}>
                   {/* Split the paragraph into parts to bold the first part */}
